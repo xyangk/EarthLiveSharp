d34ecdcd691b789ff5d97270c3e113c4479d0f41
Xiao Yang <y.shaw@qq.com>
2015-12-16 16:14:11 +0800

    testing

---
+++ wallpaper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+__auther__ = 'xiao'
+
+import re
+import urllib2
+import time
+import json
+import commands
+
+def download_img():
+    url_temp = 'http://himawari8.nict.go.jp/img/D531106/latest.json'
+    request_temp = urllib2.Request(url_temp)
+    response_temp = urllib2.urlopen(request_temp)
+    data_temp = response_temp.read()
+
+    #decode json
+    json_dec = json.JSONDecoder()
+    json_result = json_dec.decode(data_temp)
+    date_ = str(json_result['date'])
+
+    # get date
+    pattern = re.compile(r'(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)')
+    result = re.search(pattern, str(date_))
+    if result:
+        year = result.group(1)
+        month = result.group(2)
+        day = result.group(3)
+        hour = result.group(4)
+        minute = result.group(5)
+        second = result.group(6)
+    else:
+        pass
+
+    #Download picture
+    url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/thumbnail/550/"+ \
+        year+"/"+month+"/"+day+"/"+hour+minute+second+"_0_0.png"
+    request_img = urllib2.Request(url)
+    response_img = urllib2.urlopen(request_img)
+    data_img = response_img.read()
+    picname = '/home/xiao/Pictures/himawari8/Earth.png'
+    with open(picname, 'wb') as fp:
+        fp.write(data_img)
+
+    return picname
+
+def set_wallpaper():
+    picpath = download_img()
+    time.sleep(30)#wait for download
+    commands.getstatusoutput('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % (picpath))
+    print 'Done.'
+
+if __name__ == '__main__':
+    while True:
+        set_wallpaper()
+        time.sleep(600)
